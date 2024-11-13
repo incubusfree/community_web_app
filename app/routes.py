6dd069e4a602d58d32d4b2040fc95d191eba3a7d
incubusfree <85297811+incubusfree@users.noreply.github.com>
2024-11-13 13:00:29 +0200

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,32 @@
 
+from flask import render_template, url_for, redirect, request
+from app import app, db
+from app.models import Post, User
+from flask_login import login_user, login_required, current_user
+
+@app.route('/')
+@app.route('/home')
+def home():
+    posts = Post.query.all()
+    return render_template('index.html', posts=posts)
+
+@app.route('/post/<int:post_id>', methods=['GET', 'POST'])
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', post=post)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and user.password == password:
+            login_user(user)
+            return redirect(url_for('home'))
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
