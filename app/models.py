3f11874ce74ed98b11892e16095f49659bd75aff
incubusfree <85297811+incubusfree@users.noreply.github.com>
2024-11-13 13:20:49 +0200

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,7 +1,8 @@
 from datetime import datetime
 from app import db
 from flask_login import UserMixin
-
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(120), unique=True, nullable=False)
