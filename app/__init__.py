06b5c38eab4208aa7b96b7cc705220a17d0a5d02
incubusfree <85297811+incubusfree@users.noreply.github.com>
2024-11-13 13:06:41 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,17 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your_secret_key'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
+db = SQLAlchemy()
 
-from app import routes
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('config.Config')
+    db.init_app(app)
 
+    # Initialize the LoginManager
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+    login_manager.login_view = 'login'
+
+    from app import routes, models
+    return app
