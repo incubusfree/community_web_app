892dcde7af6f524d4873323e9c4dfc4ae0ed4a89
incubusfree <85297811+incubusfree@users.noreply.github.com>
2024-11-13 13:19:41 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # config.py
 class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'your_secret_key'
+    SECRET_KEY = 'Mezosasa_7'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'  # SQLite database file
 
