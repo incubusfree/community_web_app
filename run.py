a313083bfbf17e7cab3238be2d54857d3a19f14b
incubusfree <85297811+incubusfree@users.noreply.github.com>
2024-11-13 13:22:14 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,16 @@
 from app import app
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from config import Config
+from models import db
 
-if __name__ == '__main__':
+app = Flask(__name__)
+app.config.from_object(Config)
+db.init_app(app)
+
+@app.route('/')
+def home():
+    return "Welcome to Reddit Clone!"
+
+if __name__ == "__main__":
     app.run(debug=True)
-
